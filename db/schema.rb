00562447ef48eab33f62da05aabfdbe0d0cc22e4
Salman Ahmad <salman_sv@yahoo.com>
2016-08-14 23:07:25 +0000

    sections

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160814181559) do
+ActiveRecord::Schema.define(version: 20160814183025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +33,6 @@ ActiveRecord::Schema.define(version: 20160814181559) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "sections", ["course_id"], name: "index_sections_on_course_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
